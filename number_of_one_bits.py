ef7ff28d2a7e9af3bdd639c75cf8374e9e26c347
alfredleo <alfredleo@gmail.com>
2015-03-13 04:10:39 +0500

    Refactoring for pylint compliance.

--- number_of_one_bits.py
+++ number_of_one_bits.py
@@ -1,60 +1,60 @@
+# encoding: utf-8
+# module number_of_one_bits
+"""
+Write a function that takes an unsigned integer and returns the number of '1' bits it
+has (also known as the Hamming weight). For example, the 32-bit integer '11' has binary
+representation 00000000000000000000000000001011, so the function should return 3.
+"""
 from utils import test_speed
 
 __author__ = 'Alfred'
 
-"""
-Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known
-as the Hamming weight). For example, the 32-bit integer '11' has binary representation
-00000000000000000000000000001011, so the function should return 3.
-"""
 
-
-class Solution:
+class Solution(object):
     """
-    >>> s = Solution()
-    >>> print s.by_wegner(342423334234234423423L)
+    >>> print Solution.by_wegner(342423334234234423423L)
     30
-    >>> print s.by_string(342423334234234423423L)
+    >>> print Solution.by_string(342423334234234423423L)
     30
-
-    >>> print s.hammingWeight(11)
+    >>> print Solution.hamming_weight(11)
     3
     """
 
-    # @param n, an integer
-    # @return an integer
-    def hammingWeight(self, n):
-        return self.by_string(n)
-        # return self.by_wegner(n)
-
-
-    def by_string(self, n):
+    @staticmethod
+    def hamming_weight(num):
+        """ Starting function for algorithms to call
+        :param num: an integer
+        :return: an integer
         """
-        Algorithm by string
-        """
-        return (bin(n)).count('1')
+        # return Solution.by_string(num)
+        return Solution.by_wegner(num)
 
-    def by_wegner(self, n):
+    @staticmethod
+    def by_string(num):
+        """ Algorithm by string
         """
-        Wegner 1960 algorithm implementation
+        return (bin(num)).count('1')
+
+    @staticmethod
+    def by_wegner(num):
+        """ Wegner 1960 algorithm implementation
         """
         count = 0
-        while n != 0:
-            n &= n - 1
+        while num != 0:
+            num &= num - 1
             count += 1
         return count
 
 
-objectives = ['performance', 'test']
-objective = objectives[0]
+OBJECTIVES = ['performance', 'test']
+OBJECTIVE = OBJECTIVES[1]
 
-if objective == objectives[0]:
-    s = Solution()
-    print('by_string: ')
-    test_speed(s.by_string)
-    print('by_wagner: ')
-    test_speed(s.by_wegner)
-elif objective == objectives[1] and __name__ == '__main__':
+if OBJECTIVE == OBJECTIVES[0]:
+    print 'by_string: '
+    test_speed(Solution.by_string)
+    print 'by_wagner: '
+    test_speed(Solution.by_wegner)
+elif OBJECTIVE == OBJECTIVES[1] and __name__ == '__main__':
     import doctest
 
     doctest.testmod(verbose=True)
